07cb46f9b73d3661f484a22b634c432b0f7a035d
Riccardo Gulin <riccardo@nucleode.com>
2022-09-07 09:25:23 +0200

    Adds yesterday's stuff

---
+++ src/api/users/index.js
@@ -0,0 +1,97 @@
+import express from "express"
+import createError from "http-errors"
+import { adminOnlyMiddleware } from "../../lib/auth/admin.js"
+import { basicAuthMiddleware } from "../../lib/auth/basic.js"
+import UsersModel from "./model.js"
+
+const usersRouter = express.Router()
+
+usersRouter.post("/", async (req, res, next) => {
+  try {
+    const newUser = new UsersModel(req.body) // here mongoose validation happens
+    const { _id } = await newUser.save() // here the validated record is saved
+    res.status(201).send({ _id })
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/", basicAuthMiddleware, async (req, res, next) => {
+  try {
+    const users = await UsersModel.find()
+    res.send(users)
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/me", basicAuthMiddleware, async (req, res, next) => {
+  try {
+    res.send(req.user)
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.put("/me", basicAuthMiddleware, async (req, res, next) => {
+  try {
+    const updatedUser = await UsersModel.findByIdAndUpdate(req.user._id, req.body, { new: true, runValidators: true })
+    if (updatedUser) {
+      res.send(updatedUser)
+    } else {
+      next(createError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.delete("/me", basicAuthMiddleware, async (req, res, next) => {
+  try {
+    await UsersModel.findByIdAndDelete(req.user._id)
+    res.status(204).send()
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/:userId", basicAuthMiddleware, async (req, res, next) => {
+  try {
+    const user = await UsersModel.findById(req.params.userId)
+    if (user) {
+      res.send({ currentRequestingUser: req.user, user })
+    } else {
+      next(createError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.put("/:userId", basicAuthMiddleware, adminOnlyMiddleware, async (req, res, next) => {
+  try {
+    const updatedUser = await UsersModel.findByIdAndUpdate(req.params.userId, req.body, { new: true, runValidators: true })
+    if (updatedUser) {
+      res.send(updatedUser)
+    } else {
+      next(createError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.delete("/:userId", basicAuthMiddleware, adminOnlyMiddleware, async (req, res, next) => {
+  try {
+    const deletedUser = await UsersModel.findByIdAndDelete(req.params.userId)
+    if (deletedUser) {
+      res.status(204).send()
+    } else {
+      next(createError(404, `User with id ${req.params.userId} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+export default usersRouter
